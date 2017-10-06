c40a90a31b93690143ca29e387a30a2f42364393
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-06 16:17:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-session_start();
-
-
+	session_start();
+	
+	if((count($_GET) != 0 && !(isset($_GET['controle']) && isset($_GET['action']))) {
+		require('./V/erreur404.tpl');
+	}
+	else {
+		if((!isset($_SESSION['profil'])) || count($_GET) == 0) {
+			
+		}
+	}
 ?>
