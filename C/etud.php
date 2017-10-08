788411f6954289e33e50df2c9ba9494b2c6aaaa9
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-08 21:33:13 +0200

    Update etud.php

--- C/etud.php
+++ C/etud.php
@@ -1,1 +1,28 @@
-
+<?php
+/* etud.php
+ * Controller des étudiants
+ */
+	function ident() {
+		$user=isset($_POST['user'])?trim($_POST['user']):'';
+		$user=isset($_POST['pwd'])?trim($_POST['pwd']):'';
+		$msg='';
+		$profil = array();
+		
+		require("./M/eleveDB.php");
+		
+		if(count($_POST) == 0) {
+			require("./V/eleve/ident.tpl");
+		}
+		else {
+			if(verifSyntaxIdent($user, $pwd, $err) && verifIdent($user, $pwd, $profil)) {
+				$_SESSION['profil'] = $profil;
+				$nextURL = "index.php?controle=etudiant&action=accueil";
+				header("Location:" . $nextURL);
+			}
+			else {
+				$msg = $err;
+				require("./V/eleve/ident.tpl");
+			}
+		}
+	}
+?>
