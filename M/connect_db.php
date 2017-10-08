f8d1943577732c15079963e8b3a2ef549f057c68
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-08 20:34:07 +0200

    Update connect_db.php

--- M/connect_db.php
+++ M/connect_db.php
@@ -1,1 +1,13 @@
+<?php
+	require("config_db.php");
 
+	try {
+		$strConnect = 'mysql:host=$host;dbname=$db';
+		$pdo = new PDO($strConnect, $login, $password); // Instanciation de la connexion à la base
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Renvoie des exceptions en cas d'echec
+	}
+	catch(PDOException $e) {
+		$msg = 'Erreur PDO dans ' . $e.getFile() . ' Ligne ' . $e.getLine() . ' : ' . $e.getMessage();
+		die($msg);
+	}
+?>
