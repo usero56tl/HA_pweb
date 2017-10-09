ddcc1ea9d4b048f629e31db58626dd39de631826
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-09 13:07:37 +0200

    Update prof.php

--- C/prof.php
+++ C/prof.php
@@ -1,1 +1,34 @@
-
+<?php
+/* prof.php
+ * Controller des professeurs
+ */
+	function ident() {
+		$loginIdent=isset($_POST['user'])?trim($_POST['user']):'';
+		$pwdIdent=isset($_POST['pwd'])?trim($_POST['pwd']):'';
+		$msg='';
+		$profil = array();
+		
+		require("./M/profDB.php");
+		
+		if(count($_POST) == 0) {
+			require("./V/prof/ident.tpl");
+		}
+		else {
+			if(verifSyntaxIdent($loginIdent, $pwdIdent, $err) && verifIdent($user, $pwd, $profil)) {
+				$_SESSION['profil'] = $profil;
+				$nextURL = "index.php?controle=prof&action=accueil";
+				header("Location:" . $nextURL);
+			}
+			else {
+				$msg = $err;
+				require("./V/prof/ident.tpl");
+			}
+		}
+	}
+	function verifSyntaxIdent($loginIdent, $pwdIdent, &$err) {
+		// TODO preg_match ...
+	}
+	function accueil() {
+		require("./V/eleve/accueil.tpl");
+	}
+?>
