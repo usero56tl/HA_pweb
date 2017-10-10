fee7c7aa6fce90ca58bde737c899d186b78873f6
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-10 22:36:08 +0200

    Update eleveDB.php

--- M/eleveDB.php
+++ M/eleveDB.php
@@ -1,6 +1,12 @@
 <?php
 	function verifIdent($login, $pwd, &$profil) {
 		require("./M/connect_db.php");
-		// TODO
+		
+		// Requête du login
+		$queryIdent = $pdo->prepare('SELECT login_etu FROM etudiant WHERE login_etu = :logEtu AND pass_etu = :passEtu');
+		$queryIdent->execute(['logEtu' => $login, 'passEtu' => $pwd]);
+		
+		// Si l'étudiant existe renvoie true
+		return $queryIdent->rowCount();
 	}
 ?>
