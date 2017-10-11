b144282daf9b518f44c69ab8f03fa27c6edbcc4a
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-11 14:36:52 +0200

    Update eleveDB.php

--- M/eleveDB.php
+++ M/eleveDB.php
@@ -3,12 +3,20 @@
 		require("./M/connect_db.php");
 		
 		// Requête du login
-		$queryIdent = $pdo->prepare('SELECT login_etu FROM etudiant WHERE login_etu = :logEtu AND pass_etu = :passEtu');
+		$queryIdent = $pdo->prepare('SELECT login_etu, nom, prenom, email, num_grpe,  
+		FROM etudiant 
+		WHERE login_etu = :logEtu AND pass_etu = :passEtu');
+		
 		$queryIdent->execute(['logEtu' => $login, 'passEtu' => $pwd]);
 		
 		// Si l'étudiant existe renvoie true
 		if($queryIdent->rowCount()) {
-			$profil['user'] = $queryIdent->fetchALL(PDO::FETCH_BOTH);
+			$retourQuery = $queryIdent->fetchAll(PDO::FETCH_BOTH);
+			$profil['user'] = $retourQuery[0]['login_etu'];
+			$profil['nom'] = $retourQuery[0]['nom'];
+			$profil['prenom'] = $retourQuery[0]['prenom'];
+			$profil['email'] = $retourQuery[0]['email'];
+			$profil['num_grpe'] = $retourQuery[0]['num_grpe'];
 			$queryIdent->closeCursor();
 			return true;
 		}
