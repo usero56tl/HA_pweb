edabee9b449d97179ead355aca244e629cccf3d0
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-20 15:44:42 +0200

    Update utilisateur.php

--- C/utilisateur.php
+++ C/utilisateur.php
@@ -6,11 +6,11 @@
  function verifSyntaxIdent($loginIdent, $pwdIdent, &$err) {
 		if(!preg_match("[a-zA-Z0-9]{2,30}", $loginIdent)) {
 			$err = "Erreur de syntaxe du login";
-			return false;
+			//return false;
 		}
 		if(!preg_match("[a-zA-Z0-9]{2,30}", $pwdIdent)) {
 			$err = "Erreur de syntaxe du mot de passe";
-			return false;
+			//return false;
 		}
 		
 		return true;
