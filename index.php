6c63731562cde49f5758f12054f461997b518128
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-21 23:19:56 +0200

    Update index.php
    - Permet de vérifier si l'utilisateur est autorisé à effectuer des opérations sans être connecté

--- index.php
+++ index.php
@@ -5,6 +5,39 @@
 		require('./V/erreur404.tpl');
 	}
 	else {
+		if((!isset($_SESSION['profil']))) {
+			if(count($_GET) == 0) {
+				// Choix controle permettra de savoir si l'utilisateur est un prof ou un eleve
+				require("./V/choixControle.html");
+			}
+			else {
+				// Opérations interdites
+				if($_GET['action'] != 'ident') {
+					require('./V/erreur404.tpl');
+				}
+				
+				// Opérations autorisées sans login
+				$controle = $_GET['controle'];
+				$action = $_GET['action'];
+			}
+		}
+		else {
+			if(isset($_GET['controle']) && isset($_GET['action'])) {
+				$controle = $_GET['controle'];
+				$action = $_GET['action'];
+			}
+		}
+		require('./C/' . $controle . '.php');
+		$action();
+	}
+?>
+/*<?php
+	session_start();
+	
+	if((count($_GET) != 0 && !(isset($_GET['controle']) && isset($_GET['action'])))) {
+		require('./V/erreur404.tpl');
+	}
+	else {
 		if((!isset($_SESSION['profil'])) || count($_GET) == 0) {
 			
 			// Choix controle permettra de savoir si l'utilisateur est un prof ou un eleve
@@ -20,4 +53,4 @@
 		require('./C/' . $controle . '.php');
 		$action();
 	}
-?>
+?>*/
