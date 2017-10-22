9fa8ccbda1e1e9f4af8c7520a848d9ee95401424
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-22 19:36:19 +0200

    Update eleveDB.php

--- M/eleveDB.php
+++ M/eleveDB.php
@@ -14,7 +14,14 @@
 		
 		// Si l'étudiant existe renvoie true
 		if(mysqli_num_rows ($res) > 0) {
+			
+			// Récupère le profil de l'étudiant
 			$profil = mysqli_fetch_assoc($res);
+            
+            // Inscris dans la base que l'étudiant est connecté
+            $queryIdentOK = sprintf("UPDATE etudiant SET bConnect = 1 WHERE login_etu='%s' AND pass_etu='%s'", $login, $pwd);
+            mysqli_query($link, $queryIdentOK)
+                or die (utf8_encode("erreur de requête : ") . $queryIdentOK);
 			return true;
 		} 
 		else {
@@ -22,4 +29,16 @@
 			return false;
 		}
 	}
+
+	function deconnectDB($login, $pwd) {
+        require("./M/connect_db.php");
+        
+        $queryDeco = "UPDATE etudiant 
+        SET bConnect = 0 
+        WHERE login_etu='%s' AND pass_etu='%s'";
+        
+        $queryFinale = sprintf($queryDeco, $login, $pwd);
+        mysqli_query($link, $queryFinale)
+            or die(utf8_encode("erreur de requête : ") . $queryFinale);
+    }
 ?>
