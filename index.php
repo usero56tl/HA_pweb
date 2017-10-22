cd29e23dfab148cee5124d068fc075d8312d87a8
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-22 13:45:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -31,26 +31,3 @@
 		$action();
 	}
 ?>
-/*<?php
-	session_start();
-	
-	if((count($_GET) != 0 && !(isset($_GET['controle']) && isset($_GET['action'])))) {
-		require('./V/erreur404.tpl');
-	}
-	else {
-		if((!isset($_SESSION['profil'])) || count($_GET) == 0) {
-			
-			// Choix controle permettra de savoir si l'utilisateur est un prof ou un eleve
-			require("./V/choixControle.html");
-		}
-		else {
-			if(isset($_GET['controle']) && isset($_GET['action'])) {
-				$controle = $_GET['controle'];
-				$action = $_GET['action'];
-			}
-		}
-		
-		require('./C/' . $controle . '.php');
-		$action();
-	}
-?>*/
