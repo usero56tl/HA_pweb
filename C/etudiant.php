bf28b5a93680e802fce6bda3cf400e8f0b172c01
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-10-26 19:58:43 +0200

    Update etudiant.php

--- C/etudiant.php
+++ C/etudiant.php
@@ -10,10 +10,11 @@
 		
 		require("./M/eleveDB.php");
 		require("./C/utilisateur.php");
-		
-		if(count($_POST) == 0) {
-			require("./V/etudiant/ident.tpl");
-		}
+        
+        if(count($_POST) == 0) {
+            // Charge le formulaire au début
+            require("./V/accueil.tpl");
+        }
 		else {
 			// Vérifie la syntaxe des entrées
 			if(verifSyntaxIdent($loginIdent, $pwdIdent, $err)) {
@@ -22,7 +23,7 @@
 				if(verifIdent($loginIdent, $pwdIdent, $profil)) {
 					$_SESSION['profil'] = $profil;
 					$_SESSION['statut'] = "etudiant";
-					$nextURL = "index.php?controle=utilisateur&action=accueil";
+					$nextURL = "index.php?controle=etudiant&action=bienvenue";
 					header("Location:" . $nextURL);
 				}
 				else {
@@ -31,11 +32,15 @@
 			}
 			else {
 				$msg = $err;
+                // Réaffiche la page de login en cas d'erreur
+                require("./V/accueil.tpl");
 			}
-			// Réaffiche la page de login en cas d'erreur
-			require("./V/etudiant/ident.tpl");
 		}
 	}
+
+    function bienvenue() {
+        require("./V/accueil.tpl");
+    }
 
 	function deconnectEtu() {
         require("./M/eleveDB.php");
