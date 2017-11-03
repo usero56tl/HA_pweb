afeab0161a7be4e7287394dcd3fc33efcafa32d6
Kryddd <32571260+Kryddd@users.noreply.github.com>
2017-11-03 23:26:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	
+
 	if((count($_GET) != 0 && !(isset($_GET['controle']) && isset($_GET['action'])))) {
 		require('./V/erreur404.tpl');
 	}
@@ -12,21 +12,22 @@
 			}
 			else {
 				// Opérations interdites
-				if($_GET['action'] != 'ident') {
+				if($_GET['action'] != 'ident' && $_GET['action'] != 'inscription') {
 					require('./V/erreur404.tpl');
+                    require("./V/choixControle.html");
 				}
 			}
 		}
 		else {
-			if(count($_GET) == 0) {
+            if(count($_GET) == 0) {
                 // Si l'utilisateur connecté va sur l'index sans paramètres
-                $controle = $_SESSION['profil']['statut'];
+                $controle = $_SESSION['statut'];
                 $action = "bienvenue";
                 require('./C/' . $controle . '.php');
                 $action();
             }
+			
 		}
-		
 		$controle = $_GET['controle'];
 		$action = $_GET['action'];
 		
